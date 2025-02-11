92d0fca125ad67554df90364159b030bb93796d2
moizxox <mianmoiz899@gmail.com>
2025-02-11 16:54:39 +0500

    Reponse Integrated

--- src/components/main/InputSection.jsx
+++ src/components/main/InputSection.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { useContext, useState } from "react";
 import { IoMdSend } from "react-icons/io";
+import { Context } from "../../context/Context";
 
 const InputSection = () => {
+  const { setRecentInput, sendRequest } = useContext(Context);
+  const [input, setInput] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setRecentInput(input);
+    sendRequest(input);
+    setInput("");
+  };
+
   return (
-    <form className="w-full max-w-[800px] mx-auto flex border border-accent rounded-full p-3">
+    <form
+      className="w-full max-w-[800px] mx-auto flex border border-accent rounded-full p-3"
+      onSubmit={handleFormSubmit}
+    >
       <input
+        value={input}
         type="text"
         className="flex-grow outline-none"
         placeholder="What you wanna search..."
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
       />
       <button type="submit" className="cursor-pointer">
         <IoMdSend size={24} />
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,18 @@
 import InputSection from "./InputSection";
 import StartedScreen from "./StartedScreen";
 import ResponseBox from "./ResponseBox";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const Main = () => {
+  const { resultScreen } = useContext(Context);
   return (
     <main className="w-full px-5 relative h-screen flex flex-col">
       <section className="p-3">
         <h1 className="text-3xl text-accent font-medium">Gemini</h1>
       </section>
       <section className="max-w-[800px] w-full h-full mx-auto overflow-y-auto p-4 custom-scroll">
-        {/* <ResponseBox /> */}
-        <StartedScreen />
+        {resultScreen ? <ResponseBox /> : <StartedScreen />}
       </section>
       <div className="sticky bottom-0 bg-light-primary dark:bg-dark-primary w-full py-4">
         <div className="max-w-[800px] mx-auto">
